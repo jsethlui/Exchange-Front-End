afd1f42db2959326ca5759d4bf9c0f5b0c68308f
Jeremy Louie <43184783+jsethlui@users.noreply.github.com>
2020-07-25 00:28:52 -0700

    Minor front end changes

--- Exchange/Exchange/ViewController.swift
+++ Exchange/Exchange/ViewController.swift
@@ -8,40 +8,6 @@
 
 import UIKit
 
-extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
-	// height of each event cell
-	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-		return CGSize(width: collectionView.frame.width * 0.85, height: collectionView.frame.width)
-	}
-	
-	// total number of event cells
-	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return 6
-	}
-		
-	// color of background of event cell
-	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! EventCell
-		cell.backgroundColor = UIColor(red: 238.0 / 255.0, green: 120.0 / 255.0, blue: 47.0 / 255.0, alpha: 1.0)
-		return cell
-	}
-	
-	// snapping cells after done scrolling (TO DO: FIX)
-	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-		let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-		let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
-		
-		var offset = targetContentOffset.pointee
-		let index = (offset.x + scrollView.contentInset.left) / (cellWidthIncludingSpacing)
-		let roundedIndex = round(index)
-		
-		offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
-		targetContentOffset.pointee = offset
-		let generator = UIImpactFeedbackGenerator(style: .light)
-		generator.impactOccurred()
-	}
-}
-
 class ViewController: UIViewController {
 	static var pageCount = 0
 
@@ -51,7 +17,6 @@ class ViewController: UIViewController {
 		layout.scrollDirection = .horizontal
 		
 		let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-		collectionView.translatesAutoresizingMaskIntoConstraints = false
 		collectionView.register(EventCell.self, forCellWithReuseIdentifier: "cell")
 		self.view.addSubview(collectionView)
 		return collectionView
@@ -106,6 +71,16 @@ class ViewController: UIViewController {
 				break
 		}
 	}
+	
+	// dividing line between event data and carousel cells
+	fileprivate lazy var horizontalLine: UIView = {
+		let horizontalLine = UIView()
+		horizontalLine.frame.size.height = 1.0
+		horizontalLine.frame.size.width = self.view.bounds.width - 25
+		horizontalLine.backgroundColor = UIColor(red: 130.0 / 255.0, green: 130.0 / 255.0, blue: 130.0 / 255.0, alpha: 1.0)
+		self.view.addSubview(horizontalLine)
+		return horizontalLine
+	}()
 
 	// centers collection view and adds color
 	fileprivate func setUpCollectionView() {
@@ -114,13 +89,13 @@ class ViewController: UIViewController {
 	
 		collectionView.backgroundColor = UIColor(red: 239.0 / 255.0, green: 240.0 / 255.0, blue: 241.0 / 255.0, alpha: 1.0)
 		collectionView.translatesAutoresizingMaskIntoConstraints = false
-		collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+		collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 230).isActive = true
 		collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
 		collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
 		collectionView.heightAnchor.constraint(equalToConstant: view.frame.height * 0.85).isActive = true
 		collectionView.showsHorizontalScrollIndicator = false
 		collectionView.showsVerticalScrollIndicator = false
-		collectionView.heightAnchor.constraint(equalToConstant: view.frame.width * 0.85).isActive = true
+		collectionView.heightAnchor.constraint(equalToConstant: view.frame.width * 1.1).isActive = true
 	}
 	
 	// sets constraints for event name, location, and distance
@@ -137,12 +112,59 @@ class ViewController: UIViewController {
 		eventDistanceLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 125).isActive = true
 		eventDistanceLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
 	}
+	
+	// sets constraints for dividing horizontal line between event data and carousel
+	fileprivate func setUpHorizontalLine() {
+		horizontalLine.translatesAutoresizingMaskIntoConstraints = false
+//		horizontalLine.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+		horizontalLine.center = CGPoint(x: self.view.bounds.width / 2, y: 200)
+//		horizontalLine.center = self.view.center
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		setUpEventData()
 		setUpCollectionView()
+		setUpHorizontalLine()
 	}
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+	// height of each event cell
+	func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+		return CGSize(width: collectionView.frame.width * 0.85, height: collectionView.frame.height * 1.1)
+	}
+	
+	// total number of event cells
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 6
+	}
+		
+	// color of background of event cell
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! EventCell
+		return cell
+	}
+	
+	// snapping cells after done scrolling (TO DO: FIX)
+	func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+		let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+		let cellWidthIncludingSpacing = layout.itemSize.width + layout.minimumLineSpacing
+		
+		var offset = targetContentOffset.pointee
+		let index = (offset.x + scrollView.contentInset.left) / (cellWidthIncludingSpacing)
+		let roundedIndex = round(index)
+		
+		offset = CGPoint(x: roundedIndex * cellWidthIncludingSpacing - scrollView.contentInset.left, y: scrollView.contentInset.top)
+		targetContentOffset.pointee = offset
+	}
+	
+	// if scrolling is done, haptic feedback
+	func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+		if !(decelerate) {
+			let generator = UIImpactFeedbackGenerator(style: .light)
+			generator.impactOccurred()
+		}
+	}
+}
